c745738400539fcebde9f39f74d095a5f7542c7f
Megan Warnock <megandwarnock@gmail.com>
2017-04-21 11:11:25 -0700

    update user interface with output and html

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,17 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctors = function(medicalIssue, doctors) {
+  doctors.forEach(function(doctor){
+    $('.showDoctors').append("<p>" + doctor.profile + "</p>");
+  });
+};
 
 $(document).ready(function() {
   var doctor = new Doctor();
-  $('#doctorForm').submit(function() {
-   event.preventDefault();
-   var medicalIssue = $('#medicalIssue').val();
-   doctor.getDoctors(medicalIssue);
+    $('#doctorForm').submit(function() {
+      event.preventDefault();
+      var medicalIssue = $('#medicalIssue').val();
+      $('#medicalIssue').val("");
+      doctor.getDoctors(medicalIssue, displayDoctors);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,10 +4,11 @@ function Doctor(){
 }
 
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+      displayDoctors(medicalIssue, result.data);
+      console.log(result.data);
     })
    .fail(function(error){
       console.log("fail");
@@ -15,3 +16,22 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
 };
 
 exports.doctorModule = Doctor;
+
+
+// Doctor.prototype.getDoctors = function(medicalIssue) {
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+//    .then(function(result) {
+//       console.log(result);
+//     })
+//    .fail(function(error){
+//       console.log("fail");
+//     });
+// };
+
+
+
+
+// .then(function(result) {
+//    displayDoctors(medicalIssue, result.data[1].profile);
+//    console.log(result.data[1].profile);
+//  })
