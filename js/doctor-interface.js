489c6b266a64dfee07fcb63c580f27209a01676c
Megan Warnock <megandwarnock@gmail.com>
2017-04-21 20:56:36 -0700

    add location from api and update styles

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,7 +4,7 @@ var displayDoctors = function(medicalIssue, doctors) {
   console.log(medicalIssue);
   if ((doctors.length > 0) && (medicalIssue !== "")) {
     doctors.forEach(function(doctor){
-      $('.showDoctors').append("<div class='panel panel-info'><div class='panel-heading'><h4> Dr. " + doctor.profile.first_name + " " + doctor.profile.last_name + "</h4></div><div class='panel-body'><div class='row'><div class='col-md-6'><img src='" + doctor.profile.image_url + "' width='200px'></div><div class='col-md-6'> Bio: " + doctor.profile.bio + "</div></div></div>");
+      $('.showDoctors').append("<div class='panel panel-info'><div class='panel-heading'><h4 class='tab'> Dr. " + doctor.profile.first_name + " " + doctor.profile.last_name + " - " + doctor.practices[0].visit_address.street + "</h4><p>" + "number: " + doctor.practices[0].phones[0].number +"</p>" + "</div><div class='panel-body'><div class='row'><div class='col-md-3'><img src='" + doctor.profile.image_url + "'width='200px'></div><div class='col-md-9'> Bio: " + doctor.profile.bio + "</div></div></div>");
     });
   } else {
     $('.showDoctors').append("<p>Sorry there is no doctor near by for that ailment</p>");
